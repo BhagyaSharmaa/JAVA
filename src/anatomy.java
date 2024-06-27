04638b5c359ba1cd7ccae26d91b81f3f32b620f2
BhagyaSharmaa <bhagya210.be22@chitkara.edu.in>
2024-06-27 09:45:34 +0530

    this commit here is the first one

---
+++ src/anatomy.java
@@ -0,0 +1,54 @@
+//For function to work we need them to be included within the class Main.
+//Functions in Java work in similar ways as in C++.
+
+//FILE NAME FOR A JAVA FILE SHOULD BE THE SAME AS THE NAME FOR THE DECLARED CLASS.
+//AND THE DECLARED CLASS SHOULD BE INCLUDED IN THE MAIN CLASS/METHOD.
+//AND WHEN YOU WANT TO RUN THE CODE, YOU ALWAYS RUN THE FILE CONTAINING THE MAIN CLASS.
+
+/*
+class Main{
+* return_type Name(){
+*
+* }
+* OR
+* void Name(){
+* }
+*}
+*/
+// ALL THE INFORMATION WE NEED FOR A JAVA CODE/PROGRAM BEFORE WE START
+// WITH THE DSA IMPLEMENTATION.
+/*
+* Certainly. I'll provide a more detailed explanation of the process and some additional information:
+
+1. Writing the code:
+   - Ensure your code is in a file named `Main.java`. The filename must match the class name (in this case, `Main`) and have the `.java` extension.
+
+2. Compiling:
+   - The `javac` command compiles your Java source code into bytecode.
+   - Command: `javac Main.java`
+   - If successful, this creates a `Main.class` file in the same directory.
+   - If there are errors, the compiler will show them. Fix any errors and recompile.
+
+3. Running:
+   - The `java` command runs your compiled program.
+   - Command: `java Main`
+   - This starts the Java Virtual Machine (JVM) and executes your program.
+
+Additional information:
+
+- Java Development Kit (JDK):
+  - You need the JDK installed on your system to compile and run Java programs.
+  - It includes the `javac` compiler and `java` runtime.
+
+- Classpath:
+  - If your program uses external libraries or multiple class files, you may need to set the classpath.
+  - Use the `-cp` or `-classpath` option with the `java` command.
+
+- Package declaration:
+  - If your class is part of a package, the directory structure must match the package structure.
+  - You'd need to compile and run the program slightly differently.
+
+- IDEs:
+  - Integrated Development Environments like IntelliJ IDEA, Eclipse, or NetBeans can simplify this process.
+  - They often handle compilation and running with a single click.
+*/
---
+++ src/bhagya.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class bhagya{
+    public static void sharma(){
+        System.out.println("Jai Siya Ram");
+    }
+}
